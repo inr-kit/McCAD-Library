f909e945b58f86c2e2a736c23192ac2660a024c0
moatazharb <moataz.harb@kit.edu>
2019-08-23 15:44:12 +0200

    changing linux flag to default and correcting OCC CMake file

--- main.cpp
+++ main.cpp
@@ -8,15 +8,19 @@
 #include "decomposition.hpp"
 
 int main (){
-    std::string fileName;
-    std::cout << "Specify the path to the step file: " << std::endl;
-    getline(std::cin, fileName);
+  std::string inputFileName, successFileName, failFileName;
+    std::cout << "Specify the path to the input step file: " << std::endl;
+    getline(std::cin, inputFileName);
+    std::cout << "Specify the path to save the resulting step file: " << std::endl;
+    getline(std::cin, successFileName);
+    std::cout << "Specify the path to save the rejected solids'sstep file: " << std::endl;
+    getline(std::cin, failFileName);
     
     // Load the input file.
     std::cout << "***************************" << std::endl;
     std::cout << "** Loading the STEP file **" << std::endl;
     std::cout << "***************************" << std::endl;
-    McCAD::IO::STEPReader reader{fileName};
+    McCAD::IO::STEPReader reader{inputFileName};
     auto inputData = reader.getInputData();
 
     // Start decomposition.
@@ -29,10 +33,10 @@ int main (){
 
     // Write output STEP files.
     std::cout << "****************************" << std::endl;
-    std::cout << "** Saving to STEP file **" << std::endl;
+    std::cout << "**  Saving to STEP file   **" << std::endl;
     std::cout << "****************************" << std::endl;
-    McCAD::IO::STEPWriter successWriter{"resultSolids.stp", outputData_success};
-    McCAD::IO::STEPWriter failWriter{"rejectedsubSolids.stp", outputData_fail};
+    McCAD::IO::STEPWriter successWriter{successFileName, outputData_success};
+    McCAD::IO::STEPWriter failWriter{failFileName, outputData_fail};
     
     return 0;
 }
