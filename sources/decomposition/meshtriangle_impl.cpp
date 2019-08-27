3f522853bdeaed9e0f450fee5d6f84f895a0f9cc
moatazharb <moataz.harb@kit.edu>
2019-08-27 18:42:28 +0200

    rebased on develop. restructring libraries. Still working on CMake files. DO NOT BUILD YET

--- sources/decomposition/meshtriangle_impl.cpp
+++
@@ -1,9 +0,0 @@
-// McCAD
-#include "meshtriangle_impl.hpp"
-
-void
-McCAD::Decomposition::MeshTriangle::Impl::initiate(const TopoDS_Face& face){
-  Bnd_Box boundingBox;
-  BRepBndLib::Add(face, boundingBox);
-  boundingBox.SetGap(0.0);
-}
--- sources/geometry/Coordinates/Coordinates.ipp
+++
@@ -1,7 +0,0 @@
-#include "coord_type.hpp"
-#include "coord_type_const.hpp"
-#include "coord_type_util.hpp"
-
-#include "Coord.ipp"
-#include "Coord2D.ipp"
-#include "Coord3D.ipp"
