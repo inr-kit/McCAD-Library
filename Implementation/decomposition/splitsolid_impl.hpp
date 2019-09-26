b1be06ea86070b227b8b8d91ba36264d06adf289
Wegmann <rh4370@partner.kit.edu>
2019-09-26 11:36:46 +0200

    changed signature of function

--- Implementation/decomposition/splitsolid_impl.hpp
+++ Implementation/decomposition/splitsolid_impl.hpp
@@ -4,7 +4,7 @@
 // C++
 #include <array>
 #include <filesystem>
-#include <string>
+#include <optional>
 // McCAD
 #include "splitsolid.hpp"
 #include "solid_impl.hpp"
@@ -35,34 +35,37 @@
 #include <TopLoc_Location.hxx>
 
 namespace McCAD::Decomposition{
-  class SplitSolid::Impl {
-  public:
-    Impl() = default;
+    class SplitSolid::Impl {
+    public:
+        Impl() = default;
 
-    Bnd_OBB bndBox;
-    Standard_Real boxSquareLength;
-    TopoDS_Shape boundingBox;
+        Bnd_OBB bndBox;
+        Standard_Real boxSquareLength;
+        TopoDS_Shape boundingBox;
 
-    Standard_Boolean perform(Geometry::Solid::Impl& solidImpl,
-			     Standard_Integer indexSplitSurface = 0);
-    void createOBBSolid(const Bnd_OBB& OBB, TopoDS_Solid& solid);
-    Standard_Boolean split(const TopoDS_Solid& solid,
-			   const TopoDS_Face& splitFace,
-			   std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList);
-    void calculatePoints(const TopoDS_Face& splitFace,
-			 gp_Pnt& positivePoint,
-			 gp_Pnt& negativePoint);
-    Standard_Boolean splitWithBoxes(const TopoDS_Solid& solid,
-				    TopoDS_Shape& firstBox,
-				    TopoDS_Shape& secondBox,
-				    std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList);
-    Standard_Boolean checkRepair(std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList,
-				 Standard_Real tolerance = 1.0e-4);
-    Standard_Boolean rebuildSolidFromShell(const TopoDS_Shape& solid,
-					   TopoDS_Solid& resultSolid,
-					   Standard_Real tolerance = 1.0e-2,
-					   Standard_Real tolerance2 = 1.0e-4);
-  };
+        Standard_Boolean perform(const TopoDS_Solid& solid,
+                     const std::shared_ptr<Geometry::BoundSurface>& surface,
+                     std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList);
+        void calculateBoundingBox(const TopoDS_Solid& solid);
+        Standard_Boolean split(const TopoDS_Solid& solid,
+                   const TopoDS_Face& splitFace,
+                   std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList);
+        void calculatePoints(const TopoDS_Face& splitFace,
+                 gp_Pnt& positivePoint,
+                 gp_Pnt& negativePoint);
+        Standard_Boolean splitWithBoxes(const TopoDS_Solid& solid,
+                        TopoDS_Shape& firstBox,
+                        TopoDS_Shape& secondBox,
+                        std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList);
+        Standard_Boolean checkRepair(std::unique_ptr<TopTools_HSequenceOfShape>& subSolidsList,
+                     Standard_Real tolerance = 1.0e-4);
+
+    private:
+        std::optional<TopoDS_Solid> rebuildSolidFromShell(
+                const TopoDS_Shape& solid,
+                Standard_Real tolerance = 1.0e-2,
+                Standard_Real massTolerance = 1.0e-4);
+    };
 }
 
 #endif //SPLITSOLID_IMPL_HPP
