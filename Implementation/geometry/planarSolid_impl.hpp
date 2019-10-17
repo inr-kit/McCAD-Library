31bfe99dbdd5fa2065f068c96c637f2ff358b6da
moatazharb <moataz.harb@kit.edu>
2019-10-17 09:26:24 +0200

    adding new planarsolid class (continue(

---
+++ Implementation/geometry/planarSolid_impl.hpp
@@ -0,0 +1,18 @@
+#ifndef PLSOLID_IMPL_HPP
+#define PLSOLID_IMPL_HPP
+
+// C++
+// McCAD
+#include "planarSolid.hpp"
+#include "solid_impl.hpp"
+// OCC
+
+namespace McCAD::Geometry{
+  class PLSolid::Impl {
+  public:
+    Impl();
+    ~Impl();
+  };
+}
+
+#endif //PLSOLID_IMPL_HPP
