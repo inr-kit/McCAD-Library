6ecd7896a0750ebdd0cff065a7794569c1ab9d70
moatazharb <moataz.harb@kit.edu>
2020-01-07 18:20:40 +0100

    fixed creation of cylinder solid. working on trimming

--- Implementation/decomposition/torusConvertor.cpp
+++ Implementation/decomposition/torusConvertor.cpp
@@ -5,14 +5,13 @@
 #include "ShapeView.hpp"
 #include "boundSurfacePlane_impl.hpp"
 #include "surfaceObjCerator.hpp"
+#include "SolidSplitter.hpp"
 //OCC
 #include <TopTools_HSequenceOfShape.hxx>
 #include <BRepPrimAPI_MakeCylinder.hxx>
 #include <gp_Ax2.hxx>
 #include <gp_Vec.hxx>
 #include <gp_Dir.hxx>
-#include <BRepBuilderAPI_MakeShell.hxx>
-#include <BRepBuilderAPI_MakeSolid.hxx>
 #include <STEPControl_Writer.hxx>
 
 void
@@ -30,7 +29,7 @@ McCAD::Decomposition::TorusConvertor::operator()(Geometry::Solid::Impl& solidImp
 TopoDS_Shape
 McCAD::Decomposition::TorusConvertor::convertTorusToCylinder(
         const TopoDS_Shape& shape, Standard_Real scaleFactor){
-    std::vector<BRepAdaptor_Surface> planesList;
+    std::vector<TopoDS_Face> planesList;
     Standard_Real radius;
     for(const auto& face : detail::ShapeView<TopAbs_FACE>{shape}){
         if (BRepAdaptor_Surface(face).GetType() == GeomAbs_Plane){
@@ -42,16 +41,19 @@ McCAD::Decomposition::TorusConvertor::convertTorusToCylinder(
     }
     if (planesList.size() != 2) return shape;
     // create cylinder
-    gp_Vec vector(planesList[0].Plane().Location(), planesList[1].Plane().Location());
-    gp_Ax2 axis(planesList[0].Plane().Location(), gp_Dir(vector));
+    gp_Vec vector(BRepAdaptor_Surface(planesList[0]).Plane().Location(),
+            BRepAdaptor_Surface(planesList[1]).Plane().Location());
+    gp_Ax2 axis(BRepAdaptor_Surface(planesList[0]).Plane().Location(), gp_Dir(vector));
     axis.Translate(0.5*(1 - scaleFactor)*vector);
-    TopoDS_Face cylinder = BRepPrimAPI_MakeCylinder(axis, radius,
-                                                    scaleFactor*vector.Magnitude());
-    createSolid(cylinder, planesList);
+    TopoDS_Solid cylinder = BRepPrimAPI_MakeCylinder(axis, radius,
+                                                     scaleFactor*vector.Magnitude());
+    auto newSolid = retrieveSolid(cylinder, planesList);
+    if(!newSolid.has_value()) return shape;
     //debug
     STEPControl_Writer writer7;
     writer7.Transfer(cylinder, STEPControl_StepModelType::STEPControl_AsIs);
     writer7.Transfer(shape, STEPControl_StepModelType::STEPControl_AsIs);
+    writer7.Transfer(*newSolid, STEPControl_StepModelType::STEPControl_AsIs);
     Standard_Integer kk = 0;
     std::string filename = "/home/mharb/Documents/McCAD_refactor/examples/bbox/torCyl";
     std::string suffix = ".stp";
@@ -62,19 +64,19 @@ McCAD::Decomposition::TorusConvertor::convertTorusToCylinder(
     filename += suffix;
     writer7.Write(filename.c_str());
     //debug
-    return shape;
+    return newSolid;
 }
 
-void
-McCAD::Decomposition::TorusConvertor::createSolid(
-        TopoDS_Face& cylinder, std::vector<BRepAdaptor_Surface>& planesList){
-    std::cout << "create solid" << std::endl;
-    auto firstSurface = GeomAdaptor_Surface(planesList[0].Surface()).Surface();
-    auto secondSurface = GeomAdaptor_Surface(planesList[0].Surface()).Surface();
-    auto thirdSurface = GeomAdaptor_Surface(BRepAdaptor_Surface(cylinder).Surface()).Surface();
-    try {BRepBuilderAPI_MakeSolid(BRepBuilderAPI_MakeShell(firstSurface).Shell(),
-                                  BRepBuilderAPI_MakeShell(secondSurface).Shell(),
-                                  BRepBuilderAPI_MakeShell(thirdSurface).Shell());
-    } catch (...) {};
+std::optional<TopoDS_Solid>
+McCAD::Decomposition::TorusConvertor::retrieveSolid(
+        TopoDS_Solid& cylinder, std::vector<TopoDS_Face>& planesList){
+    auto solidObj = Solid::Impl{};
+    solidObj.initiate(cylinder);
+    solidObj.createOBB();
+    solidObj.calcMeshDeflection();
+    auto firstExtFace = generateExtendedFace(planesList[0], solidObj.boxDiagonalLength);
+    auto solids = SolidSplitter{}(cylinder, solidObj.obb, firstExtFace);
+    if(!solids) return std::nullopt;
+    return solids.first;
 }
 
