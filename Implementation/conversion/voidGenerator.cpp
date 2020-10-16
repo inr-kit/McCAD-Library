174efa58aa258128b2e2680e0b4e7a636a4d2196
moatazharb <moataz.harb@kit.edu>
2020-10-16 17:16:38 +0200

    starting void generator

---
+++ Implementation/conversion/voidGenerator.cpp
@@ -0,0 +1,9 @@
+// McCAD
+#include "voidGenerator.hpp"
+
+McCAD::Conversion::Impl::VoidGenerator::VoidGenerator(
+        const std::shared_ptr<TopTools_HSequenceOfShape> solidsList){
+
+}
+
+McCAD::Conversion::Impl::VoidGenerator::~VoidGenerator(){}
