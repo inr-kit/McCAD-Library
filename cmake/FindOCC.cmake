9a15bf4ed04be09480a8cab49d9de0658ce819f2
moatazharb <moataz.harb@kit.edu>
2021-02-10 14:16:12 +0100

    some changes in order of building and allowing for Win installation

--- cmake/FindOCC.cmake
+++ cmake/FindOCC.cmake
@@ -59,6 +59,81 @@ if(Linux_OS)
     if(DEFINED OCC_INCLUDE_DIRS)
         include_directories(${OCC_INCLUDE_DIRS})
     endif(DEFINED OCC_INCLUDE_DIRS)
+else(Linux_OS)
+    if(DEFINED OCC_ROOT)
+        set(OCC_LIBRARY_DIRS "${OCC_ROOT}/win64/vc14/lib")
+        set(OCC_INCLUDE_DIRS "${OCC_ROOT}/inc")
+        set(OpenCASCADE_LIBRARIES   "${OCC_LIB_DIR}/TKBin.lib"
+                            "${OCC_LIB_DIR}/TKBinL.lib"
+                            "${OCC_LIB_DIR}/TKBinTObj.lib"
+                            "${OCC_LIB_DIR}/TKBinXCAF.lib"
+                            "${OCC_LIB_DIR}/TKBO.lib"
+                            "${OCC_LIB_DIR}/TKBool.lib"
+                            "${OCC_LIB_DIR}/TKBRep.lib"
+                            "${OCC_LIB_DIR}/TKCAF.lib"
+                            "${OCC_LIB_DIR}/TKCDF.lib"
+                            "${OCC_LIB_DIR}/TKLCAF.lib"
+                            "${OCC_LIB_DIR}/TKSTL.lib"
+                            "${OCC_LIB_DIR}/TKXMesh.lib"
+                            "${OCC_LIB_DIR}/TKernel.lib"
+                            "${OCC_LIB_DIR}/TKMath.lib"
+                            "${OCC_LIB_DIR}/TKService.lib"
+                            "${OCC_LIB_DIR}/TKTObj.lib"
+                            "${OCC_LIB_DIR}/TKXml.lib"
+                            "${OCC_LIB_DIR}/TKFeat.lib"
+                            "${OCC_LIB_DIR}/TKMesh.lib"
+                            "${OCC_LIB_DIR}/TKTopAlgo.lib"
+                            "${OCC_LIB_DIR}/TKXmlL.lib"
+                            "${OCC_LIB_DIR}/TKFillet.lib"
+                            "${OCC_LIB_DIR}/TKMeshVS.lib"
+                            "${OCC_LIB_DIR}/TKShHealing.lib"
+                            "${OCC_LIB_DIR}/TKV3d.lib"
+                            "${OCC_LIB_DIR}/TKXmlTObj.lib"
+                            "${OCC_LIB_DIR}/TKG2d.lib"
+                            "${OCC_LIB_DIR}/TKXmlXCAF.lib"
+                            "${OCC_LIB_DIR}/TKG3d.lib"
+                            "${OCC_LIB_DIR}/TKOffset.lib"
+                            "${OCC_LIB_DIR}/TKVRML.lib"
+                            "${OCC_LIB_DIR}/TKXSBase.lib"
+                            "${OCC_LIB_DIR}/TKGeomAlgo.lib"
+                            "${OCC_LIB_DIR}/TKOpenGl.lib"
+                            "${OCC_LIB_DIR}/TKSTEP.lib"
+                            "${OCC_LIB_DIR}/TKXCAF.lib"
+                            "${OCC_LIB_DIR}/TKGeomBase.lib"
+                            "${OCC_LIB_DIR}/TKSTEP209.lib"
+                            "${OCC_LIB_DIR}/TKHLR.lib"
+                            "${OCC_LIB_DIR}/TKSTEPAttr.lib"
+                            "${OCC_LIB_DIR}/TKXDEIGES.lib"
+                            "${OCC_LIB_DIR}/TKIGES.lib"
+                            "${OCC_LIB_DIR}/TKPrim.lib"
+                            "${OCC_LIB_DIR}/TKSTEPBase.lib"
+                            "${OCC_LIB_DIR}/TKXDESTEP.lib")
+
+        if(BUILD_SHARED)
+            set(OCC_LIBRARIES_SHARED)
+            foreach (OCC_lib ${OpenCASCADE_LIBRARIES})
+                set(OCC_LIB "OCC_LIB-NOTFOUND")
+                find_library(OCC_LIB
+                    NAMES ${OCC_lib}
+                    HINTS ${OCC_LIBRARY_DIRS}
+                    NO_DEFAULT_PATH)
+                list(APPEND OCC_LIBRARIES_SHARED ${OCC_LIB})
+            endforeach ()
+        endif()
+        if(BUILD_STATIC)
+            set(OCC_LIBRARIES_STATIC)
+            foreach (OCC_lib ${OpenCASCADE_LIBRARIES})
+                set(OCC_LIB "OCC_LIB-NOTFOUND")
+                find_library(OCC_LIB
+                    NAMES ${OCC_lib}
+                    HINTS ${OCC_LIBRARY_DIRS}
+                    NO_DEFAULT_PATH)
+                list(APPEND OCC_LIBRARIES_STATIC ${OCC_LIB})
+            endforeach ()
+        endif()
+
+        include_directories(${OCC_INCLUDE_DIRS})
+    endif(DEFINED OCC_ROOT)
 endif(Linux_OS)
 
 message(STATUS "OCC_INCLUDE_DIRS: ${OCC_INCLUDE_DIRS}")
--- src/conversion/Implementation/BVHCreator.cpp
+++ src/conversion/Implementation/BVHCreator.cpp
@@ -54,23 +54,30 @@ McCAD::Conversion::BVHCreator::constructBVH(const std::string& fileName,
     BRep_Builder aBuilder;
     TopoDS_Compound aCompound;
     TopoDS_Shape aShape;
-    aShape = inputSolidsList->Value(1);
-    std::cout << "load file" << std::endl;
     BRepExtrema_ShapeList aFaceList;
-    for (TopExp_Explorer i(aShape, TopAbs_FACE); i.More(); i.Next()){
-        aFaceList.Append(TopoDS::Face(i.Current()));
-    }
-    std::cout << "face list" << std::endl;
     aBuilder.MakeCompound(aCompound);
-    BRepMesh_IncrementalMesh aMesher(aShape, 1.0);
-    std::cout << "mesh shape" << std::endl;
+    for (int j = 1; j <= inputSolidsList->Length(); ++j){
+        aShape = inputSolidsList->Value(j);
+        std::cout << "add solid" << std::endl;
+        for (TopExp_Explorer i(aShape, TopAbs_FACE); i.More(); i.Next()){
+            aFaceList.Append(TopoDS::Face(i.Current()));
+        }
+        std::cout << "face list" << std::endl;
+        BRepMesh_IncrementalMesh aMesher(aShape, 1.0);
+        std::cout << "mesh shape" << std::endl;
+        aBuilder.Add(aCompound, aShape);}
     BRepExtrema_TriangleSet aTriangleSet(aFaceList);
     std::cout << "triangle set, " << aTriangleSet.Size() << std::endl;
     const opencascade::handle<BVH_Tree<Standard_Real, 3> >& aBvh = aTriangleSet.BVH();
     std::cout << "construct bvh, " << aBvh->Length() <<std::endl;
+    TopoDS_Compound Leaf, Inner;
+    aBuilder.MakeCompound(Leaf);
+    aBuilder.MakeCompound(Inner);
     for (int i = 0; i < aBvh->Length(); i++){
         const BVH_Vec4i& aNodeData = aBvh->NodeInfoBuffer()[i];
+        std::cout << i << std::endl;
         if (aNodeData.x() == 0){
+            std::cout << "inner" << std::endl;
             // inner node.
             const BVH_Vec3d& aP1 = aBvh->MinPoint(aNodeData.y());
             const BVH_Vec3d& aP2 = aBvh->MaxPoint(aNodeData.y());
@@ -79,33 +86,34 @@ McCAD::Conversion::BVHCreator::constructBVH(const std::string& fileName,
             try{
                 BRepPrimAPI_MakeBox aBoxMaker(gp_Pnt(aP1.x(), aP1.y(), aP1.z()),
                                               gp_Pnt(aP2.x(), aP2.y(), aP2.z()));
-                for (TopExp_Explorer i(aBoxMaker.Shape(), TopAbs_EDGE); i.More();
-                     i.Next()){
-                    aBuilder.Add(aCompound, i.Current());
-                }
+                aBuilder.Add(Inner, aBoxMaker.Shape());
+                goto Continue;
             } catch (Standard_Failure f){}
             try{
+                std::cout << "inner, first failed" << std::endl;
                 BRepPrimAPI_MakeBox aBoxMaker(gp_Pnt(aQ1.x(), aQ1.y(), aQ1.z()),
                                               gp_Pnt(aQ2.x(), aQ2.y(), aQ2.z()));
-                for (TopExp_Explorer i(aBoxMaker.Shape(), TopAbs_EDGE); i.More();
-                     i.Next()){
-                    aBuilder.Add(aCompound, i.Current());
-                }
+                aBuilder.Add(Inner, aBoxMaker.Shape());
             } catch (Standard_Failure f){}
         } else{
+            std::cout << "leaf" << std::endl;
             // leaves node.
             const BVH_Vec3d& aP1 = aBvh->MinPoint(i);
             const BVH_Vec3d& aP2 = aBvh->MaxPoint(i);
             try{
                 BRepPrimAPI_MakeBox aBoxMaker(gp_Pnt(aP1.x(), aP1.y(), aP1.z()),
                                               gp_Pnt(aP2.x(), aP2.y(), aP2.z()));
-                aBuilder.Add(aCompound, aBoxMaker.Shape());
+                aBuilder.Add(Leaf, aBoxMaker.Shape());
             } catch (Standard_Failure f){}
         }
+        Continue:;
     }
-    STEPControl_Writer writer1;
-    writer1.Transfer(aCompound, STEPControl_StepModelType::STEPControl_AsIs);
-    Standard_Integer kk = 0;
-    std::string filename = "bvh.stp";
-    writer1.Write(filename.c_str());
+    STEPControl_Writer writer1, writer2;
+    writer1.Transfer(Leaf, STEPControl_StepModelType::STEPControl_AsIs);
+    std::string filename1 = "leaf.stp";
+    writer1.Write(filename1.c_str());
+    writer2.Transfer(Inner, STEPControl_StepModelType::STEPControl_AsIs);
+    std::string filename2 = "inner.stp";
+    writer2.Write(filename2.c_str());
+
 }
